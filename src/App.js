91120b27c073ea46a2b94feaad5b23b27233014e
Jaider <jaidercueto.1010@gmail.com>
2023-04-12 12:26:30 -0500

    creating routes and adding styles at App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import { Route } from 'react-router-dom';
+import NavBar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import Band from './views/Band/Band';
+import Services from './views/Services/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='appWrapper'>
+        <NavBar/>
+          <Route exact path = '/' component = { Home } />
+          <Route path = '/about' component = { About } />
+          <Route path = '/band' component = { Band } />
+          <Route path = '/services' component = { Services } />
+          <Footer/>
+    </div>        
   );
 }
 
