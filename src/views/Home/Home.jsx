717d76398a92fbea13eefbb8d89ad4b7f56a4c36
Jaider <jaidercueto.1010@gmail.com>
2023-04-13 12:52:00 -0500

    First styles on home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 
 const Home = () => {
     return(
         <main className={styles.main}>
-            <h1>Main Section</h1>
+            <div className={styles.coverWrapper}>
+                <h1>Ministerios Totalmente Consagrados</h1>
+                <h2>Welcome Church</h2>
+            </div>
+            <div className={styles.intro}>
+                <h2>
+                    MTC is a church that believes in Jesus, 
+                    a church that loves God and people.
+                </h2>
+                <p>
+                    Overwhelmed by the gift of salvation we have found
+                    in Jesus, we have a heart for authentic worship,
+                    are passionate about the local church,
+                    and are on mission to see God’s kingdom established
+                    across the earth.
+                </p>
+            </div>
+            <div className={styles.location}>
+                <h2>Where are we located?</h2>
+                <h4>Barranquilla - Colombia</h4>
+            </div>
         </main>
     )
 }
