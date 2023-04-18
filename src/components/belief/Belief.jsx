2fa71f3b9dea2361beb33ee94feea3dea48aa9e3
Jaider <jaidercueto.1010@gmail.com>
2023-04-18 16:11:55 -0500

    belief and vision components modified

--- src/components/belief/Belief.jsx
+++ src/components/belief/Belief.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import styles from './Belief.module.css'
 
 const Belief = () => {
     return (
-        <div> 
-            Belief Component
+        <div className={styles.intro}> 
+            <p><b>We believe</b> that the Bible is God’s Word. It is accurate, authoritative and applicable to our everyday lives.</p>
+
+            <p><b>We believe</b> in one eternal God who is the Creator of all things. He exists in three Persons: God the Father, God the Son and God the Holy Spirit. He is totally loving and completely holy.</p>
+
+            <p><b>We believe</b> that sin has separated each of us from God and His purpose for our lives.</p>
+
+            <p><b>We believe</b> that the Lord Jesus Christ as both God and man is the only One who can reconcile us to God. He lived a sinless and exemplary life, died on the cross in our place, and rose again to prove His victory and empower us for life.</p>
+
+            <p><b>We believe</b> that in order to receive forgiveness and the ‘new birth’ we must repent of our sins, believe in the Lord Jesus Christ, and submit to His will for our lives.</p>
+
+            <p><b>We believe</b> that in order to live the holy and fruitful lives that God intends for us, we need to be baptised in water and be filled with the power of the Holy Spirit. The Holy Spirit enables us to use spiritual gifts, including speaking in tongues.</p>
+
+            <p><b>We believe</b> in the power and significance of the Church and the necessity of believers to meet regularly together for fellowship, prayer and the ‘breaking of bread’.</p>
+
+            <p><b>We believe</b> that God has individually equipped us so that we can successfully achieve His purpose for our lives which is to worship God, fulfil our role in the Church and serve the community in which we live.</p>
+
+            <p><b>We believe</b> that God wants to heal and transform us so that we can live healthy and blessed lives in order to help others more effectively.</p>
+
+            <p><b>We believe</b> that our eternal destination of either Heaven or hell is determined by our response to the Lord Jesus Christ.</p>
+
+            <p><b>We believe</b> that the Lord Jesus Christ is coming back again as He promised.</p>
         </div>
     )
 }
--- src/components/vision/Vision.jsx
+++ src/components/vision/Vision.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from './Vision.module.css'
+import picture_One from '../../images/MTC-logo.jpg'
 
 const Vision = () => {
     return (
-        <div> 
-            Vision Component
+        <div className={styles.intro}> 
+            <img src={picture_One} alt="mtc-home" />
+                <div>
+                        <h2>
+                            MTC VISION
+                        </h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Modi dolorem sint earum voluptatum harum et, 
+                            porro numquam recusandae laboriosam tenetur
+                            cumque ex doloremque ullam voluptatibus maxime
+                            culpa tempora sequi deleniti
+                        </p>
+                </div>
         </div>
     )
 }
