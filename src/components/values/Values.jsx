492095a01a35c4451505811bb56097c6a0fe8b16
Jaider <jaidercueto.1010@gmail.com>
2023-04-19 15:13:19 -0500

    adding text to values.jsx component & modifying styles on about submenu

--- src/components/values/Values.jsx
+++ src/components/values/Values.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from './Values.module.css'
 
 const Values = () => {
     return (
-        <div> 
-            Values Component
+        <div className={styles.intro}>
+            <h4>The relationship with God</h4>
+            <p>We love God with all our heart, soul, mind, and with all our strength. He is the priority of our love, obedience and worship, above all things; if you take away God, you take away life itself (Luke 10:27).</p>
+            
+            <h4>The family</h4>
+            <p>It is a fundamental principle in the kingdom of God. The union in marriage between a man and a woman is the will of God, and keeps a society free of immorality. If the family is lost, the foundation of society is lost; that is why we care after and protect marriage, till death do them part (Genesis 1:27)</p>
+            
+            <h4>The Word of God</h4>
+            <p>Every declaration, every word and every commandment written in the Bible is the law for us, and we commit to follow it (Proverbs 3:3; 2 Timothy 3:16).</p>
         </div>
     )
 }
