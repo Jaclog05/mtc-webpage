5ebdccd17e70c28e752aaf0e20be963a65a268cf
Jaider <jaidercueto.1010@gmail.com>
2023-04-25 15:59:27 -0500

    state added to navBar component styles for tab selected/no selected

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.css"
 import logo from "../../images/MTC-logo.jpg"
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    const [selected, setSelected] = useState("home")
+
+    const handleClick = (e) => {
+        let {id} = e.target
+        setSelected(id)
+    }
+
     return(
         <nav className={styles.navBar}>
                 <Link to='/' className={styles.linkLogo}>
@@ -13,10 +20,10 @@ const NavBar = () => {
                     </div>
                 </Link>
                 <div className={styles.right}>
-                        <Link to="/" className={styles.links}>Home</Link>
-                        <Link to="/about" className={styles.links}>About Us</Link>
-                        <Link to="/band" className={styles.links}>Band</Link>
-                        <Link to="/services" className={styles.links}>Our services</Link>
+                        <Link to="/" className={selected !== 'home' ? styles.links : styles.selected} id="home" onClick={handleClick}>Home</Link>
+                        <Link to="/about" className={selected !== 'about' ? styles.links : styles.selected} id="about" onClick={handleClick}>About Us</Link>
+                        <Link to="/band" className={selected !== 'band' ? styles.links : styles.selected} id="band" onClick={handleClick}>Band</Link>
+                        <Link to="/services" className={selected !== 'services' ? styles.links : styles.selected} id="services" onClick={handleClick}>Our services</Link>
                 </div>
       </nav>
     )
